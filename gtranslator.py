44ee88301ed2e840f62fc439295dbaf290f04e64
ethan-x11 <subhamdutta96740@gmail.com>
2023-11-30 19:02:06 +0530

    .

---
+++ gtranslator.py
@@ -0,0 +1,35 @@
+from langdetect import detect, detect_langs
+from translate import Translator
+from deep_translator import GoogleTranslator
+from tqdm import tqdm
+import pandas as pd
+
+
+# Simple function to detect and translate text
+def detect_and_translate(text, target_lang='en'):
+    result_lang = detect(text)
+    
+    if result_lang == target_lang:
+        return text
+    else:
+        translator = GoogleTranslator(source='auto', target='en')
+        translate_text = translator.translate(text)
+        return translate_text
+    
+def translation_loop(textlist,target_lang='en'):
+    res = {}
+    print("Translating...")
+    df = pd.DataFrame({'Text': textlist})
+    for i, texts in tqdm(df.iterrows(), total=len(df)):
+        try:
+            text = texts['Text']
+            translated_text = detect_and_translate(text, target_lang)
+            res[i] = translated_text
+        except RuntimeError:
+            print(f'\nBroke for id {i}')
+
+    return res
+
+if __name__ == '__main__':
+    example = ["english is a good language", "নমস্কার"]
+    print(translation_loop(example, 'en'))
