ee50ba41a9065ebfd71ba65bf1b9821c9404ca4d
ethan-x11 <subhamdutta96740@gmail.com>
2023-12-04 02:19:50 +0530

    .

--- server/amazon_review.py
+++ server/amazon_review.py
@@ -2,7 +2,6 @@ import subprocess
 import datetime
 import time
 import os
-import os
 import datetime
 import pandas as pd
 import re
--- server/routers/sanly.py
+++ server/routers/sanly.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter,status
 from main_pipeline import execute_pipeline
 import schemas
-from amazon_review import fetch_reviews
 
 router = APIRouter()
 
 
 @router.post('/amazon', status_code=status.HTTP_201_CREATED, tags=['Analysis Route'])
 def predict(request: schemas.Load):
-    output = fetch_reviews(
+    output = execute_pipeline(
         url=request.aurl
     )
     # output = {"Meow"}
