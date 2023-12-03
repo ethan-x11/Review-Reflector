8a2563e7a1e92a8013d922968d7653fffb86d852
ethan-x11 <subhamdutta96740@gmail.com>
2023-12-04 01:23:16 +0530

    .

--- server/routers/sanly.py
+++ server/routers/sanly.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter,status
 from main_pipeline import execute_pipeline
 import schemas
-
+from amazon_review import fetch_reviews
 
 router = APIRouter()
 
 
 @router.post('/amazon', status_code=status.HTTP_201_CREATED, tags=['Analysis Route'])
 def predict(request: schemas.Load):
-    output = execute_pipeline(
+    output = fetch_reviews(
         url=request.aurl
     )
     # output = {"Meow"}
